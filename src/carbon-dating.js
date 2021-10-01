d50e2201496e65335c708cd566f4f92757bbb478
Andrey S. Kochergin <kchrgn@gmail.com>
2021-10-01 13:51:45 +0300

    feat: add 3 solve

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -20,7 +20,7 @@ const HALF_LIFE_PERIOD = 5730;
 export default function dateSample(sampleActivity) {
   if (isNaN(+sampleActivity)) return false;
   if (typeof (sampleActivity) != 'string') return false;
-  if (+sampleActivity > 15) return false;
+  if (+sampleActivity > 15 || +sampleActivity <= 0) return false;
   if (Number.isInteger(Math.ceil(Math.log(15/sampleActivity)/1.22e-4))) {
     return Math.ceil(Math.log(15/sampleActivity)/1.22e-4)
    } else {
--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  // if (date=='') return 'Invalid date!';
+  if (date === undefined) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date)) return 'Invalid date!';
+  let month = date.getMonth();
+  if (month == 11 || month == 0 || month == 1) return 'winter';
+  if (month == 2 || month == 3 || month ==  4) return 'spring';
+  if (month == 5 || month == 6 || month == 7) return "summer";
+  if (month == 8 || month == 9 || month == 10 ) return 'autumn'; 
 }
